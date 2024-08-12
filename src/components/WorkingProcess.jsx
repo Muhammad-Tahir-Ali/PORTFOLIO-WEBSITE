ee18ce3ac810696bb70a9d79fa6fe7b42138b7ea
Muhammad Tahir Ali <WORKFORTAHIR786@GMAIL.COM>
2024-08-12 12:58:38 +0500

    animation fix

--- src/components/WorkingProcess.jsx
+++ src/components/WorkingProcess.jsx
@@ -6,7 +6,7 @@ const WorkingProcess = () => {
   return (
     <>
     <motion.div
-    initial={{ y: -200, opacity: 0 }}
+    initial={{ y: -100, opacity: 0 }}
     whileInView={{ y: 0, opacity: 1 }}
     transition={{ duration: 0.5, delay: 0 }}
     >
